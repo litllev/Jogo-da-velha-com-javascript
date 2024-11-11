7ca54824619332855db415edee2fa31a18770b01
Victor herison <vitorherisonvhp@gmail.com>
2024-11-11 16:10:12 -0300

    front-end feito.

---
+++ script.js
@@ -0,0 +1,24 @@
+let dropdownItem = document.getElementById('dropdown-item')
+let statusDropdown = 1
+let regras = document.getElementById('regras')
+let listaDeRegras = document.getElementById('dropdown-item')
+
+function dropdown(){
+    if(statusDropdown === 0){
+
+        dropdownItem.style.display = 'block'
+        statusDropdown = 1
+        regras.innerHTML = 'Regras do jogo' + '<ion-icon name="caret-up-outline"></ion-icon>';
+        listaDeRegras.style.animation = 'fadein 1s ease-out'
+
+    }else{
+        
+        statusDropdown = 0
+        regras.innerHTML = 'Regras do jogo' + '<ion-icon name="caret-down-outline"></ion-icon>'
+        listaDeRegras.style.animation = 'fadeout 0.5s ease'
+        setTimeout(()=> dropdownItem.style.display = 'none',400)
+        
+
+    }
+    
+}
