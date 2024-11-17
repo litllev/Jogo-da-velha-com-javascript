65c3fc38cd5d2c30c8ce58fec8f52e9ccb0293f6
Victor herison <vitorherisonvhp@gmail.com>
2024-11-17 20:12:02 -0300

    Finalizado a parte logica do game

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let regras = document.getElementById('regras')
 let listaDeRegras = document.getElementById('dropdown-item')
 
 let posicoes = document.getElementsByClassName('piece')
-
+let placar = document.getElementById('placar').children
 
 function dropdown(){
     if(statusDropdown === 0){
@@ -32,40 +32,80 @@ function dropdown(){
 // Pra saber quem está jogando vou ultilizar um contador, a partir do contador vai ser possivel saber de quem é a vez
 
 let contador = 0
-let alguemGanhou
+let x = 0
+let o = 0
+let empate = 0
 function adicionarIcone(elemento){
-    if (contador % 2 === 0) {
-        elemento.innerHTML = '<img src="./imagens/close-outline.svg" alt="Marcador X" data-icone="x">'
-    }else{
-        elemento.innerHTML = '<img src="./imagens/ellipse-outline.svg" alt="Marcador O" data-icone="o">'
+    if(elemento.innerHTML === ''){
+        if (contador % 2 === 0) {
+            elemento.innerHTML = '<img src="./imagens/close-outline.svg" alt="Marcador X" data-icone="x">'
+        }else{
+            elemento.innerHTML = '<img src="./imagens/ellipse-outline.svg" alt="Marcador O" data-icone="o">'
+        }
+        contador += 1
     }
-    contador += 1
     if(contador >= 5){
         //chamando a função de verificação apartir do ponto que ja pode haver uma vitoria
         verificar()
+    }
+    if(contador === 9){
+        window.alert(`Deu empate`)
+        empate++
+        placar[2].innerHTML = 'Empates: ' + empate
+        limparTela()
     }
 }
 
 function verificar(){
     //verificando se o usuario fez uma tripla em coluna
-try {
     for(let i = 0; i<=8; i++){
-        if (posicoes[i].childNodes[0].dataset.icone === posicoes[i+3].childNodes[0].dataset.icone && posicoes[i].childNodes[0].dataset.icone === posicoes[i+6].childNodes[0].dataset.icone) {
-            return console.log(`O jogador ${posicoes[i].children[0].dataset.icone} ganhou!`)
+            try {
+                if (posicoes[i].childNodes[0].dataset.icone === posicoes[i+3].childNodes[0].dataset.icone && posicoes[i].childNodes[0].dataset.icone === posicoes[i+6].childNodes[0].dataset.icone) {
+                    return fimDoJogo(posicoes[i].children[0].dataset.icone)
+                }
+            }catch (error) {
+                
+            }
+            }
+    for (let j = 0; j <= 8; j += 3) {
+        try {
+            if (posicoes[j].childNodes[0].dataset.icone === posicoes[j+1].childNodes[0].dataset.icone && posicoes[j].childNodes[0].dataset.icone === posicoes[j+2].childNodes[0].dataset.icone) {
+                return fimDoJogo(posicoes[j].children[0].dataset.icone)
+            }
+        }catch (error) {
+            
         }
-}
-    
-for(let j = 0; j<=8; j += 3){
-    if (posicoes[j].dataset.icone === posicoes[j+1].dataset.icone === posicoes[j+2].dataset.icone) {
-           return console.log(`O jogador ${posicoes[j].dataset.icone} ganhou!`)
         }
-    }
+            try{
+                if (
+                    posicoes[0].childNodes[0].dataset.icone === posicoes[4].childNodes[0].dataset.icone && 
+                    posicoes[0].childNodes[0].dataset.icone === posicoes[8].childNodes[0].dataset.icone || 
+                    posicoes[2].childNodes[0].dataset.icone === posicoes[4].childNodes[0].dataset.icone &&
+                    posicoes[2].childNodes[0].dataset.icone === posicoes[6].childNodes[0].dataset.icone
+                    ) {
+                        return fimDoJogo(posicoes[4].children[0].dataset.icone)
+                    }
+        } catch(error){
 
-if ((posicoes[0].dataset.icone === posicoes[4].dataset.icone === posicoes[8].dataset.icone) || 
-    posicoes[2].dataset.icone === posicoes[4].dataset.icone === posicoes[6].dataset.icone) {
-        return console.log(`O jogador ${posicoes[4]} ganhou!`)
-    }
-} catch (error) {
-    
+        }
 }
+
+function fimDoJogo(jogador) {
+    window.alert(`O jogador ${jogador} Venceu`)
+    if (jogador === 'x') {
+        x++
+        placar[0].innerHTML = 'Vitorias de x: ' + x
+    } else {
+        o++
+        placar[1].innerHTML = 'Vitorias de o: ' + o
+    }
+    limparTela()
+}
+
+
+function limparTela(){
+    contador = 0
+    for (element of posicoes) {
+        element.innerHTML = ''  
+    }
 }
