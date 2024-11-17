ad7351256a4adc846efdc4fe4945f2bb87f95fad
Victor herison <vitorherisonvhp@gmail.com>
2024-11-17 15:43:13 -0300

    Logica do jogo sendo feita

--- script.js
+++ script.js
@@ -28,3 +28,44 @@ function dropdown(){
 
 // fazendo o game funcionar
 
+// logica de quem está jogando se X ou O
+// Pra saber quem está jogando vou ultilizar um contador, a partir do contador vai ser possivel saber de quem é a vez
+
+let contador = 0
+let alguemGanhou
+function adicionarIcone(elemento){
+    if (contador % 2 === 0) {
+        elemento.innerHTML = '<img src="./imagens/close-outline.svg" alt="Marcador X" data-icone="x">'
+    }else{
+        elemento.innerHTML = '<img src="./imagens/ellipse-outline.svg" alt="Marcador O" data-icone="o">'
+    }
+    contador += 1
+    if(contador >= 5){
+        //chamando a função de verificação apartir do ponto que ja pode haver uma vitoria
+        verificar()
+    }
+}
+
+function verificar(){
+    //verificando se o usuario fez uma tripla em coluna
+try {
+    for(let i = 0; i<=8; i++){
+        if (posicoes[i].childNodes[0].dataset.icone === posicoes[i+3].childNodes[0].dataset.icone && posicoes[i].childNodes[0].dataset.icone === posicoes[i+6].childNodes[0].dataset.icone) {
+            return console.log(`O jogador ${posicoes[i].children[0].dataset.icone} ganhou!`)
+        }
+}
+    
+for(let j = 0; j<=8; j += 3){
+    if (posicoes[j].dataset.icone === posicoes[j+1].dataset.icone === posicoes[j+2].dataset.icone) {
+           return console.log(`O jogador ${posicoes[j].dataset.icone} ganhou!`)
+        }
+    }
+
+if ((posicoes[0].dataset.icone === posicoes[4].dataset.icone === posicoes[8].dataset.icone) || 
+    posicoes[2].dataset.icone === posicoes[4].dataset.icone === posicoes[6].dataset.icone) {
+        return console.log(`O jogador ${posicoes[4]} ganhou!`)
+    }
+} catch (error) {
+    
+}
+}
